fff6c6cc6b6b14822aa4f981f2b8cdbee24bf9ad
youjin Cho <ud071233@gmail.com>
2023-10-22 22:33:45 +0900

    initial commit

---
+++ springproj/src/main/java/com/uujinu/springproj/SpringprojApplication.java
@@ -0,0 +1,13 @@
+package com.uujinu.springproj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringprojApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringprojApplication.class, args);
+	}
+
+}
