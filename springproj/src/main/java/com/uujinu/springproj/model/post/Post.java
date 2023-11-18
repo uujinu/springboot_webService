0f7de54ccb2514923f77a750263aeb1496c23ea4
youjin Cho <ud071233@gmail.com>
2023-11-18 18:45:57 +0900

    test

---
+++ springproj/src/main/java/com/uujinu/springproj/model/post/Post.java
@@ -0,0 +1,36 @@
+package com.uujinu.springproj.model.post;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@NoArgsConstructor
+@Getter
+@Entity
+public class Post {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @Column(length = 500, nullable = false)
+    private String title;
+
+    @Column(columnDefinition = "TEXT", nullable = false)
+    private String content;
+
+    private String author;
+
+    @Builder
+    public Post(String title, String content, String author) {
+        this.title = title;
+        this.content = content;
+        this.author = author;
+    }
+
+}
